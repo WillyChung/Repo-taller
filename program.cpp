a814d6e229e418e87cf4c8f8a62cfa8a1271c9a0
alulab14 <alulab14@inf.pucp.edu.pe>
2019-03-26 15:59:31 -0500

    Cambio al imprimir multiplo de 3

--- program.cpp
+++ program.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 	for(int i=0;i<100;i++){
 		if( i%3 == 0)
-			cout<<"Fizz"<<endl;
+			cout<<"Fizz123123123"<<endl;
 		else if (i%5==0)
 			cout<<"Buzz"<<endl;
 		else if(i%3==0 && i%5==0)
